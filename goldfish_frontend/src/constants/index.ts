b49841a2d5338ce00ddfcc5875669c029f5de3af
Artur Shirokov <artur.wiseman.co@gmail.com>
2025-05-20 16:28:36 +0100

    Updated some stuff.

--- goldfish_frontend/src/constants/index.ts
+++
@@ -1,7 +0,0 @@
-export const GOLDFISH_PACKAGE_ID =
-  "0xd7da3d972c99d9318eb56df786b8b04e120a7769d572f537d920f40334388dd6";
-export const FILE_REGISTRY_OBJECT_ID =
-  "0xa2b58dd03872c5bd0f337b13056eb50f9160848329efd9ad965f63e8aac1bc67";
-export const TABLE_OBJECT_ID = "0x9801afde129050adb0573fadfd798fa9733104d4521bb8936991e59a2ad706f0"
-export const FILE_REGISTRY_MODULE_NAME = "goldfish_backend";
-export const NETWORK = "testnet"; // Should match your WalletProvider network
--- goldfish_frontend/src/hooks/useWalrus.ts
+++ goldfish_frontend/src/hooks/useWalrus.ts
@@ -1,7 +1,13 @@
 import { WalrusClient } from "@mysten/walrus";
 import { SuiClient, getFullnodeUrl } from "@mysten/sui/client";
 import walrusWasmUrl from "@mysten/walrus-wasm/web/walrus_wasm_bg.wasm?url"; // Vite specific import
-import { NETWORK } from "../constants";
+import {
+  NETWORK,
+  SYSTEM_OBJECT_ID,
+  STAKING_POOL_ID,
+  SUBSIDIES_OBJECT_ID,
+  EXCHANGGE_IDS,
+} from "../constants";
 
 export const suiClient = new SuiClient({
   url: getFullnodeUrl("testnet"),
@@ -15,11 +21,9 @@ export const walrusClient = new WalrusClient({
     onError: (error) => console.error("Walrus Node Error:", error),
   },
   packageConfig: {
-    systemObjectId:
-      "0x6c2547cbbc38025cf3adac45f63cb0a8d12ecf777cdc75a4971612bf97fdf6af",
-    stakingPoolId:
-      "0xbe46180321c30aab2f8b3501e24048377287fa708018a5b7c2792b35fe339ee3",
-    subsidiesObjectId:
-      "0xda799d85db0429765c8291c594d334349ef5bc09220e79ad397b30106161a0af",
+    systemObjectId: SYSTEM_OBJECT_ID,
+    stakingPoolId: STAKING_POOL_ID,
+    subsidiesObjectId: SUBSIDIES_OBJECT_ID,
+    exchangeIds: EXCHANGGE_IDS,
   },
 });
